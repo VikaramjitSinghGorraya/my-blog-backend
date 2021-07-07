57a7b31c9da0f0c529a72fc5cce62a69803caa5d
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-07-07 09:54:09 -0600

    added the netlify origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(expressValidator())
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors({origin: 'https://jovial-payne-9512ac.netlify.app/'}))
 
 app.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
