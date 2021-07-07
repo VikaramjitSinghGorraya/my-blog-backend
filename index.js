b851e60bbfca02442adedf7731edd17add19ea3a
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-07-07 09:35:08 -0600

    added origin to cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(expressValidator())
-app.use(cors())
+app.use(cors({origin: 'http://localhost:3000'}))
 
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
