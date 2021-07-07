f40ffd070bece053fb86d8a41923806149e8167c
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-07-07 10:54:31 -0600

    commented the set headers

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(expressValidator())
-app.use(cors({origin: 'https://jovial-payne-9512ac.netlify.app', allowedHeaders: '*', credentials: true}))
+app.use(cors({origin: 'https://jovial-payne-9512ac.netlify.app', allowedHeaders: '*', credentials: true }))
 
-app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'https://jovial-payne-9512ac.netlify.app');
-        next();
-    })
+// app.use(function (req, res, next) {
+//         res.setHeader('Access-Control-Allow-Origin', 'https://jovial-payne-9512ac.netlify.app');
+//         next();
+//     })
 
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
