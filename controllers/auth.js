9a48b5cad94020011f94aaacc339ace939711e25
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-07-07 14:01:47 -0600

    set cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -136,8 +136,7 @@ exports.signin = (req,res) =>{
         const id = user._id
         const token = jwt.sign({id}, secret, {expiresIn: '1d'})
         res.cookie('token', token, {
-            expires  : new Date(Date.now() + (10 * 365 * 24 * 60 * 60)),
-            httpOnly: true,
+            maxAge  : 5184000000,
             secure: true
           })
         const {_id, email, name, about, createdAt} = user
